cc5e4074f99f1c4c0bd38b2681589b6d12a5e8f4
Martin Pávek <aiejrfierie@proton.me>
2025-01-16 13:43:52 +0100

    Add files via upload

---
+++ stazeni_celeho_repozitare.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# stazeni celeho tohoto repozitare v linuxovim terminalu pres primy odkaz
+wget -O sifrovaci_programy.zip "https://github.com/Sifrovaci-Programy-2/Sifrovaci-Programy/archive/refs/heads/main.zip" && unzip -t sifrovaci_programy.zip && unzip -x sifrovaci_programy.zip
+
+# a nebo lze take prikazem "git clone https://github.com/Sifrovaci-Programy/Sifrovaci_Programy"
